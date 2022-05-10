d9cd25d960aaffe28e76f6acad2ee4abe254c007
Logan Domingo <logankdomingo@gmail.com>
2022-05-09 23:24:46 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Home from './components/Home';
+import Hello from './components/Hello';
+import HelloColors from './components/HelloColors';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-4">
+      <BrowserRouter>
+        <Switch>
+          //home path
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          //hello & number path
+          <Route exact path="/:hello">
+            <Hello />
+          </Route>
+          <Route exact path="/:word/:color1/:color2">
+            <HelloColors />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
